d4c07a2d963eafdd854f896f5f974ff5e96f3585
Yaroslav Sakhno <yaroslav.saxno@yandex.ru>
2024-04-03 16:32:37 +0300

    Добавлены суперсилы и достижения

--- src/api.js
+++ src/api.js
@@ -1,4 +1,4 @@
-const baseHost = "https://wedev-api.sky.pro/api/leaderboard";
+const baseHost = "https://wedev-api.sky.pro/api/v2/leaderboard";
 
 export async function getLeaders() {
   const response = await fetch(baseHost, {
@@ -13,12 +13,13 @@ export async function getLeaders() {
   return data;
 }
 
-export async function postLeader({ name, time }) {
+export async function postLeader({ name, time, achievements }) {
   const response = await fetch(baseHost, {
     method: "POST",
     body: JSON.stringify({
       name,
       time,
+      achievements,
     }),
   });
 
--- src/pages/LeaderboadPage/LeaderboardPage.jsx
+++ src/pages/LeaderboadPage/LeaderboardPage.jsx
@@ -4,6 +4,10 @@ import { useEffect, useState } from "react";
 import { getLeaders } from "../../api";
 import cn from "classnames";
 import { Button } from "../../components/Button/Button";
+import hardModeAchievement from "./images/hardModeAchievement.svg";
+import withoutPowerUpsAchievement from "./images/withoutPowerUpsAchievement.svg";
+import notHardModeAchievement from "./images/notHardModeAchievement.svg";
+import notWithoutPowerUpsAchievement from "./images/notWithoutPowerUpsAchievement.svg";
 
 export function LeaderboardPage() {
   const [leaders, setLeaders] = useState([]);
@@ -33,6 +37,8 @@ export function LeaderboardPage() {
         <div className={styles.leadersContainer}>
           <div className={cn(styles.leaderPosition, styles.gray)}>Позиция</div>
           <div className={cn(styles.leaderName, styles.gray)}>Пользователь</div>
+          <div className={cn(styles.leaderName, styles.gray)}>Достижения</div>
+
           <div className={cn(styles.leaderTime, styles.gray)}>Время</div>
         </div>
         {leaders
@@ -41,7 +47,27 @@ export function LeaderboardPage() {
             <div className={styles.leadersContainer} key={leader.id}>
               <div className={styles.leaderPosition}># {index + 1}</div>
               <div className={styles.leaderName}>{leader.name}</div>
-              <div className={styles.leaderTime}>{leader.time}</div>
+              <div className={styles.achievements}>
+                {leader.achievements.includes(1) ? (
+                  <img src={hardModeAchievement} alt="hardModeAchievement" />
+                ) : (
+                  <img src={notHardModeAchievement} alt="notHardModeAchievement" />
+                )}
+                {leader.achievements.includes(2) ? (
+                  <img src={withoutPowerUpsAchievement} alt="withoutPowerUpsAchievement" />
+                ) : (
+                  <img src={notWithoutPowerUpsAchievement} alt="notWithoutPowerUpsAchievement" />
+                )}
+              </div>
+              <div className={styles.leaderTime}>
+                {Math.floor(leader.time / 60)
+                  .toString()
+                  .padStart("2", "0")}
+                :
+                {Math.floor(leader.time % 60)
+                  .toString()
+                  .padStart("2", "0")}
+              </div>
             </div>
           ))}
       </div>
